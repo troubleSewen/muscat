22658d16e81244747da42129e8de8758be1d48a8
Sean Yao <sean.yao@biposervice.com>
2018-02-02 16:14:12 +0800

    [sean] - refactor the ComandLineRunner Demo

--- muscat-subscription/src/main/java/com/bipo/iac/MuscatApplication.java
+++ muscat-subscription/src/main/java/com/bipo/iac/MuscatApplication.java
@@ -1,8 +1,6 @@
 package com.bipo.iac;
 
-import com.bipo.iac.model.CustomerAccount;
 import com.bipo.iac.model.EndUserAccount;
-import com.bipo.iac.repository.CustomerAccountRepository;
 import com.bipo.iac.repository.EndUserAccountRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,18 +13,17 @@ import org.springframework.context.annotation.Bean;
 import java.util.Arrays;
 
 @SpringBootApplication
-public class Application {
+public class MuscatApplication {
 
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
+    private static final Logger log = LoggerFactory.getLogger(MuscatApplication.class);
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+        ApplicationContext ctx = SpringApplication.run(MuscatApplication.class, args);
         String[] activeProfiles = ctx.getEnvironment().getActiveProfiles();
         for (String profile : activeProfiles) {
             System.out.println("Springboot on profile " + profile);
         }
     }
-
 
     @Bean
     public CommandLineRunner demo(EndUserAccountRepository repository){
@@ -34,8 +31,11 @@ public class Application {
             for (String mobileSuffix : Arrays.asList("7001,7002,7003".split(","))) {
                 EndUserAccount endUserAccount = new EndUserAccount("N#" + "1800000" + mobileSuffix, mobileSuffix, "123456");
                 repository.save(endUserAccount);
-                log.info("New account has been created : {}", endUserAccount.getName());
+                log.info("New account has been created");
             }
+
+            log.info("Load all created end user accounts:");
+            repository.findAll().forEach(endUserAccount -> log.info(endUserAccount.getName()));
         });
     }
 }
